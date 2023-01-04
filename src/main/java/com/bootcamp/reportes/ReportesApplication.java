ce1147ebabe95b5d25ccc5d7fbdb5fdaec14a576
Luis Acuña <luisandresverdugox@gmail.com>
2023-01-03 21:37:02 -0500

    Configurando Maven para generacion de .JAR

--- src/main/java/com/bootcamp/reportes/ReportesApplication.java
+++ src/main/java/com/bootcamp/reportes/ReportesApplication.java
@@ -1,10 +1,17 @@
 package com.bootcamp.reportes;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReportesApplication {
+public class ReportesApplication implements CommandLineRunner {
+
+	@Override
+	public void run(String args[]) throws Exception
+	{
+		System.out.println("Probando SpringBoot con GitHub Actions Deploy Azure Web App");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReportesApplication.class, args);
